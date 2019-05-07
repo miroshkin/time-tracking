98acff41590cf2fedc17ed3147eca5b0ddf935eb
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-05-07 11:08:59 +0300

    Added react components

---
+++ TimeTrackingApp/wwwroot/js/project/read_projects.component.js
@@ -0,0 +1,42 @@
+﻿// component that contains all the logic and other smaller components
+// that form the Read Products view
+window.ReadProductsComponent = React.createClass({
+    getInitialState: function () {
+        return {
+            products: []
+        };
+    },
+
+    // on mount, fetch all products and stored them as this component's state
+    componentDidMount: function () {
+
+        this.serverRequest = $.get("http://localhost:49986/api/projects", function (products) {
+            this.setState({
+                products: products.records
+            });
+        }.bind(this));
+    },
+
+    // on unmount, kill product fetching in case the request is still pending
+    componentWillUnmount: function () {
+        this.serverRequest.abort();
+    },
+
+    // render component on the page
+    render: function () {
+        // list of products
+        alert(this.state.products.count);
+        var filteredProducts = this.state.products;
+        $('.page-header h1').text('Read Products');
+
+        return (
+        <div className='overflow-hidden'>
+            <TopActionsComponent changeAppMode={this.props.changeAppMode} />
+
+            <ProductsTable
+        products={filteredProducts}
+        changeAppMode={this.props.changeAppMode} />
+            </div>
+);
+}
+});
