b81a34cb35e7c853f97409b0b1c4f4953f6f20ea
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-05-11 03:13:30 +0300

    Added CORS

--- TimeTrackingService/Startup.cs
+++ TimeTrackingService/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -25,6 +26,7 @@ namespace TimeTrackingService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc();
 
             services.AddDbContext<TimeTrackingServiceContext>(options =>
@@ -38,6 +40,12 @@ namespace TimeTrackingService
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
 
             app.UseMvc();
         }
